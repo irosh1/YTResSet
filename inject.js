d9aee3a4a84fcb4ff2df38bd85df0a574f16084a
Irosh <ikumara@e-vis.com.au>
2024-10-16 13:36:43 +0530

    added channel icon to the list

--- inject.js
+++ inject.js
@@ -1,83 +1,83 @@
 (function() {
-    let qualitySetSuccess = false;
-    let channelQualities = {};
-    let currentChannel = "";
-    let defaultQuality = "auto";
-  
-    // Listen for the initialization message from content.js
-    window.addEventListener("message", (event) => {
-      if (event.data.type === "YOUTUBE_QUALITY_INIT") {
-        channelQualities = event.data.channelQualities;
-        currentChannel = event.data.currentChannel;
-        defaultQuality = event.data.defaultQuality;
-        ytQualityControl();
-      }
-    });
-  
-    function setCustomQuality() {
-      const player = document.querySelector('#movie_player');
-      if (!player || typeof player.getAvailableQualityLevels !== 'function') {
-        return false;
-      }
+  let qualitySetSuccess = false;
+  let channelQualities = {};
+  let currentChannel = "";
+  let defaultQuality = "auto";
+
+  // Listen for the initialization message from content.js
+  window.addEventListener("message", (event) => {
+    if (event.data.type === "YOUTUBE_QUALITY_INIT") {
+      channelQualities = event.data.channelQualities;
+      currentChannel = event.data.currentChannel;
+      defaultQuality = event.data.defaultQuality;
+      ytQualityControl();
+    }
+  });
+
+  function setCustomQuality() {
+    const player = document.querySelector('#movie_player');
+    if (!player || typeof player.getAvailableQualityLevels !== 'function') {
+      return false;
+    }
+    
+    const availableQualities = player.getAvailableQualityLevels();
+    let targetQuality = channelQualities.hasOwnProperty(currentChannel) 
+      ? channelQualities[currentChannel].quality
+      : defaultQuality;
+
+    function getBestQuality(target, available) {
+      const qualityOrder = ['highres', 'hd2160', 'hd1440', 'hd1080', 'hd720', 'large', 'medium', 'small', 'tiny', 'auto'];
+      const targetIndex = qualityOrder.indexOf(target);
       
-      const availableQualities = player.getAvailableQualityLevels();
-      let targetQuality = channelQualities.hasOwnProperty(currentChannel) 
-        ? channelQualities[currentChannel] 
-        : defaultQuality;
-  
-      function getBestQuality(target, available) {
-        const qualityOrder = ['highres', 'hd2160', 'hd1440', 'hd1080', 'hd720', 'large', 'medium', 'small', 'tiny', 'auto'];
-        const targetIndex = qualityOrder.indexOf(target);
-        
-        if (targetIndex === -1) {
-          return available[0];
-        }
-  
-        for (let i = targetIndex; i < qualityOrder.length; i++) {
-          if (available.includes(qualityOrder[i])) {
-            return qualityOrder[i];
-          }
-        }
-  
+      if (targetIndex === -1) {
         return available[0];
       }
-  
-      const bestQuality = getBestQuality(targetQuality, availableQualities);
-      player.setPlaybackQualityRange(bestQuality, bestQuality);
-  
-      if (player.getPlayerState() === 2) {
-        player.playVideo();
+
+      for (let i = targetIndex; i < qualityOrder.length; i++) {
+        if (available.includes(qualityOrder[i])) {
+          return qualityOrder[i];
+        }
       }
-  
-      window.postMessage({ type: "QUALITY_SET_SUCCESS", quality: bestQuality }, "*");
-      return true;
+
+      return available[0];
     }
-  
-    function ytQualityControl() {
-      if (!qualitySetSuccess) {
-        qualitySetSuccess = setCustomQuality();
-      }
+
+    const bestQuality = getBestQuality(targetQuality, availableQualities);
+    player.setPlaybackQualityRange(bestQuality, bestQuality);
+
+    if (player.getPlayerState() === 2) {
+      player.playVideo();
     }
-  
-    const events = ['yt-navigate-start', 'yt-navigate-finish'];
-    events.forEach(event => {
-      document.addEventListener(event, () => {
-        qualitySetSuccess = false;
-        setTimeout(ytQualityControl, 2000);
-      });
+
+    window.postMessage({ type: "QUALITY_SET_SUCCESS", quality: bestQuality }, "*");
+    return true;
+  }
+
+  function ytQualityControl() {
+    if (!qualitySetSuccess) {
+      qualitySetSuccess = setCustomQuality();
+    }
+  }
+
+  const events = ['yt-navigate-start', 'yt-navigate-finish'];
+  events.forEach(event => {
+    document.addEventListener(event, () => {
+      qualitySetSuccess = false;
+      setTimeout(ytQualityControl, 2000);
     });
-  
-    const originalPushState = history.pushState;
-    history.pushState = function() {
-      originalPushState.apply(this, arguments);
-      qualitySetSuccess = false;
-      setTimeout(ytQualityControl, 2000);
-    };
-  
-    const originalReplaceState = history.replaceState;
-    history.replaceState = function() {
-      originalReplaceState.apply(this, arguments);
-      qualitySetSuccess = false;
-      setTimeout(ytQualityControl, 2000);
-    };
-  })();
+  });
+
+  const originalPushState = history.pushState;
+  history.pushState = function() {
+    originalPushState.apply(this, arguments);
+    qualitySetSuccess = false;
+    setTimeout(ytQualityControl, 2000);
+  };
+
+  const originalReplaceState = history.replaceState;
+  history.replaceState = function() {
+    originalReplaceState.apply(this, arguments);
+    qualitySetSuccess = false;
+    setTimeout(ytQualityControl, 2000);
+  };
+})();
